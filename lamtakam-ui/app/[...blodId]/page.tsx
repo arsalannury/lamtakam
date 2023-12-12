5e820099ce388591860b0813553c77dbe67cf20d
arsalannury <arsiiiland@yahoo.com>
2023-12-12 16:34:07 +0330

    🎨 start complete blog module

---
+++ lamtakam-ui/app/[...blodId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+const BlogDetail = async () => {
+     
+  return (
+    <div>BlogDetail</div>
+  )
+}
+
+export default BlogDetail;
---
+++ lamtakam-ui/components/Feed/BlogDetail/BlogDetail.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+
+
+const BlogDetail = () => {
+  return (
+    <div>BlogDetail</div>
+  )
+}
+
+export default BlogDetail
